f6352b67edf365718b418f7414a9436393f6b4df
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2021-05-26 21:33:52 +0300

    Fixed database path issue and a test function

--- upnotify-bot/src/main/java/utils/DatabaseUtils.java
+++ upnotify-bot/src/main/java/utils/DatabaseUtils.java
@@ -65,8 +65,9 @@ interface DatabaseUtilsInterface {
 public class DatabaseUtils implements DatabaseUtilsInterface
 {
     public Connection connection = null;
-    public String url = "jdbc:sqlite:upnotify-bot/src/main/resources/upnotify.db";
-
+    //public String url = "jdbc:sqlite:upnotify-bot/src/main/resources/upnotify.db"; // for vscode
+    //public String url = "jdbc:sqlite:src/main/resources/upnotify.db"; // for eclipse
+    public String url = "jdbc:sqlite:" + this.getClass().getResource("/upnotify.db");
     private static DatabaseUtils single_instance = null;
 
     public static DatabaseUtils getDatabaseUtils() {
